47f89476a6b296b6c52a125e94c1917f3a2a8988
xjm <925271237@qq.com>
2018-06-11 07:49:24 +0800

    “c#组件开发

---
+++ com开发/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+
+
+namespace Components
+{
+    public class Component1 : Component
+    {
+        private int _id;
+        private string _name;
+        private DateTime _createDateTime;
+
+        // 在Property窗口中为灰色显示。
+        public int Id
+        {
+            get { return _id; }
+        }
+
+        // 在Property窗口中可以设置值。 
+        public string name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        //在Property窗口中不可见
+        public DateTime CreateDateTime
+        {
+            set { _createDateTime = value; }
+        }
+    }
+}
